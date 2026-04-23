0862eedf905e9d8e07d26625535afe654d910d13
mahdi7800 <mdavoudi47@gmail.com>
2026-04-23 15:52:15 +0330

    پروژه شمارنده زمان

---
+++ WindowsFormsApp7/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace WindowsFormsApp7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.day = new System.Windows.Forms.Label();
+            this.hour = new System.Windows.Forms.Label();
+            this.min = new System.Windows.Forms.Label();
+            this.sec = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(106, 144);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.Location = new System.Drawing.Point(25, 60);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(96, 20);
+            this.dateTimePicker1.TabIndex = 1;
+            this.dateTimePicker1.Value = new System.DateTime(2026, 2, 26, 0, 0, 0, 0);
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dateTimePicker2.Location = new System.Drawing.Point(165, 60);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.ShowUpDown = true;
+            this.dateTimePicker2.Size = new System.Drawing.Size(84, 20);
+            this.dateTimePicker2.TabIndex = 2;
+            this.dateTimePicker2.Value = new System.DateTime(2026, 2, 26, 0, 0, 0, 0);
+            // 
+            // day
+            // 
+            this.day.AutoSize = true;
+            this.day.Location = new System.Drawing.Point(36, 261);
+            this.day.Name = "day";
+            this.day.Size = new System.Drawing.Size(23, 13);
+            this.day.TabIndex = 3;
+            this.day.Text = "روز";
+            // 
+            // hour
+            // 
+            this.hour.AutoSize = true;
+            this.hour.Location = new System.Drawing.Point(103, 261);
+            this.hour.Name = "hour";
+            this.hour.Size = new System.Drawing.Size(35, 13);
+            this.hour.TabIndex = 4;
+            this.hour.Text = "ساعت";
+            // 
+            // min
+            // 
+            this.min.AutoSize = true;
+            this.min.Location = new System.Drawing.Point(169, 261);
+            this.min.Name = "min";
+            this.min.Size = new System.Drawing.Size(32, 13);
+            this.min.TabIndex = 5;
+            this.min.Text = "دقیقه";
+            // 
+            // sec
+            // 
+            this.sec.AutoSize = true;
+            this.sec.Location = new System.Drawing.Point(221, 261);
+            this.sec.Name = "sec";
+            this.sec.Size = new System.Drawing.Size(28, 13);
+            this.sec.TabIndex = 6;
+            this.sec.Text = "ثانیه";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(33, 220);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 25);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "00";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(100, 220);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(38, 25);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "00";
+            this.label6.Click += new System.EventHandler(this.Label6_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(163, 220);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(38, 25);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "00";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(218, 220);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 25);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "00";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.Disable;
+            this.ClientSize = new System.Drawing.Size(292, 300);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.sec);
+            this.Controls.Add(this.min);
+            this.Controls.Add(this.hour);
+            this.Controls.Add(this.day);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.button1);
+            this.HelpButton = true;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        protected internal System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Label day;
+        private System.Windows.Forms.Label hour;
+        private System.Windows.Forms.Label min;
+        private System.Windows.Forms.Label sec;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WindowsFormsApp7/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+
+            timer1.Enabled = true;
+
+			CalculateTimeDifference();
+		}
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+			CalculateTimeDifference();
+			
+
+		}
+
+		private void CalculateTimeDifference()
+		{
+			DateTime selected = dateTimePicker1.Value.Date + dateTimePicker2.Value.TimeOfDay;
+
+			TimeSpan diff = DateTime.Now - selected;
+
+
+
+			if (diff.TotalSeconds < 0) {
+				label1.Text = (-1 * diff.Seconds).ToString();
+				label7.Text = (-1 * diff.Minutes).ToString();
+				label6.Text = (-1 * diff.Hours).ToString();
+				label5.Text = (-1 * diff.Days).ToString();
+
+			}
+			else {
+				label1.Text = diff.Seconds.ToString();
+				label7.Text = diff.Minutes.ToString();
+				label6.Text = diff.Hours.ToString();
+				label5.Text = diff.Days.ToString();
+			}
+
+		}
+	}
+}
